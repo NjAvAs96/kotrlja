d247d69ebc3b9ba2b721a2a16806867b0d427144
Vanja <vanja.pp96@gmail.com>
2023-04-24 01:29:35 +0200

    bio update

--- src/js/_parts/info.js
+++ src/js/_parts/info.js
@@ -2,6 +2,10 @@ import Swiper from 'swiper/bundle';
 
 const info = {
 	init: function() {
+		this.infoSlider();
+		this.bio();
+	},
+	infoSlider: function() {
 		const swiper = new Swiper('.js-info', {
 			effect: 'fade',
 			speed: 1000,
@@ -16,6 +20,24 @@ const info = {
 			  prevEl: '.js-info-next',
 			},
 		});
+	},
+	bio: function() {
+		let open = document.querySelectorAll('.js-bio-open');
+		let close = document.querySelectorAll('.js-info-close');
+		let controls = document.querySelector('.js-info-controls');
+		open.forEach(btn => {
+			btn.addEventListener('click', function() {
+				btn.nextElementSibling.classList.toggle('active');
+				controls.style.zIndex = 0;
+			})
+		});
+		close.forEach(element => {
+			element.addEventListener('click', () => {
+				element.parentElement.classList.remove('active');
+				controls.removeAttribute('style');
+
+			})
+		})
 	}
 }
 
